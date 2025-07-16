4f8134e06db0f187da651ab90570d3b575e6d433
sparshsing <sparshsing96@gmail.com>
2025-07-16 12:28:31 +0530

    dev basepath fix

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === 'development';
 const nextConfig: NextConfig = {
   output: "export",
   eslint: {
     ignoreDuringBuilds: false,
     dirs: ['src/app'], // Directories to run ESLint on
   },
-  basePath: '/projects/traffic-simulator', // Set the subpath
-  assetPrefix: '/projects/traffic-simulator',
+  basePath: isDev ? '' : '/projects/traffic-simulator', // Set the subpath
+  assetPrefix: isDev ? '' :'/projects/traffic-simulator',
   trailingSlash: true, // Ensures proper URL handling,
   env: {
-    NEXT_PUBLIC_BASE_PATH: '/projects/traffic-simulator'
+    NEXT_PUBLIC_BASE_PATH: isDev ? '' : '/projects/traffic-simulator'
   }
 };
 
