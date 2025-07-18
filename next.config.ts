05f5e7168c8228dd42de8d09d122e34675b69258
sparshsing <sparshsing96@gmail.com>
2025-07-18 20:35:58 +0530

    next config update

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
 import type { NextConfig } from "next";
 
-const isDev = false;//process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'development';
+const basePath = '/projects/traffic-simulator'
+
 const nextConfig: NextConfig = {
   output: "export",
   eslint: {
     ignoreDuringBuilds: false,
     dirs: ['src/app'], // Directories to run ESLint on
   },
-  basePath: isDev ? '' : '/projects/traffic-simulator', // Set the subpath
-//   assetPrefix: isDev ? '' :'/projects/traffic-simulator',
+  basePath: isDev ? '' : basePath, // Set the subpath
+  assetPrefix: isDev ? '' : basePath,
 //   trailingSlash: true, // Ensures proper URL handling,
   env: {
-    NEXT_PUBLIC_BASE_PATH: isDev ? '' : '/projects/traffic-simulator'
+    NEXT_PUBLIC_BASE_PATH: isDev ? '' : basePath
   }
 };
 
